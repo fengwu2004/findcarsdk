33bd9bc11504d5064b6801bd7f261b69a7e13d4f
晏立 <yanli@yellfun.com>
2017-01-24 09:48:06 +0800

    将楼层控件抽成独立控件 bug fix

--- modules/FloorListControl.js
+++ modules/FloorListControl.js
@@ -6,7 +6,7 @@ define(function (require, exports, module) {
 
 
 
-    var FloorListControl = function(mapMD, blueTooth, comm) {
+    var FloorListControl = function(mapMD, blueTooth, comm, map) {
 
         var oFloorCtx = jsLib('#floorCtx');    // 包含生成楼层的父Div块
 
