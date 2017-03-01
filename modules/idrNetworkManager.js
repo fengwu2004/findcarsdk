989f9dd794cfe5d07d327def9afb1fa2e61dac40
晏立 <yanli@yellfun.com>
2017-03-01 11:18:38 +0800

    test

--- modules/idrNetworkManager.js
+++ modules/idrNetworkManager.js
@@ -39,7 +39,7 @@ define(function (require, exports, module) {
 
             },
 
-            success:function (data) {
+            success:function (response) {
 
                 if (response != null && response.code == "success") {
 
@@ -50,7 +50,7 @@ define(function (require, exports, module) {
                 }
             },
 
-            error:function (data) {
+            error:function (response) {
 
                 if (typeof failed === "function") {
 
