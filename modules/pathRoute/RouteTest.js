b7b092e945d699142e210741ba0c31327f641fd8
晏立 <yanli@yellfun.com>
2017-03-14 16:00:58 +0800

    寻路算法

--- modules/pathRoute/RouteTest.js
+++ modules/pathRoute/RouteTest.js
@@ -27,7 +27,7 @@ define(function (require, exports, module) {
 
         var result = router.search(0, p1, 0, p2, false, null)
 
-        var pathResult = null;
+        var pathResult = {};
 
         pathResult.distance = result.getDistance();
 
