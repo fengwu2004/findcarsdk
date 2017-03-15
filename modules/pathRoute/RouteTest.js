095c6780cf386232e9c2b8b8c8ed283a07b76ed4
晏立 <yanli@yellfun.com>
2017-03-15 17:02:54 +0800

    寻路算法

--- modules/pathRoute/RouteTest.js
+++ modules/pathRoute/RouteTest.js
@@ -11,7 +11,51 @@ define(function (require, exports, module) {
 
         var p2 = {x:770, y:269, floorId:'14428254382890016'}
 
-        var router = new idrRouter(null, null)
+        var floorList = [
+            {
+                "collectLineList":[ ],
+                "mapModifyTime":1489550481000,
+                "floorIndex":0,
+                "width":1120,
+                "quyuList":[ ],
+                "unitModifyTime":1489550481000,
+                "recordTime":1458111565000,
+                "id":"14428254382890016",
+                "title":"F1",
+                "height":530,
+                "collectModifyTime":1489144711000,
+                "name":"F1",
+                "unitList":[ ],
+                "modifyTime":1489550481000,
+                "regionId":"14428254382730015"
+            },
+            {
+                "collectLineList":[
+
+                ],
+                "mapModifyTime":1483498009000,
+                "floorIndex":1,
+                "width":752,
+                "quyuList":[
+
+                ],
+                "groupNames":"null",
+                "unitModifyTime":1489144483000,
+                "recordTime":1455758385000,
+                "id":"14557583851000004",
+                "title":"F2",
+                "height":373,
+                "collectModifyTime":1479695320000,
+                "name":"F2",
+                "unitList":[
+
+                ],
+                "modifyTime":1489144483000,
+                "regionId":"14428254382730015"
+
+            }
+
+        var router = new idrRouter('14428254382730015', floorList)
 
         var carNavi = false
 
