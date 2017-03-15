33c0493f95c703d73e2fedd649f6171727c58c0a
晏立 <yanli@yellfun.com>
2017-03-15 14:45:12 +0800

    寻路算法

--- modules/idrRouter.js
+++ modules/idrRouter.js
@@ -7,11 +7,39 @@ define(function (require, exports, module) {
 
     var RegionPath = require('./pathRoute/idrRegionPathData')
 
-    function idrRouter(regionId) {
+    function idrRouter(regionId, floorList) {
 
         var _regionId = regionId
 
+        var _floorList = floorList
+
         var _pathSearch = new PathSearch(RegionPath)
+
+        function getFloorIndex(floorId) {
+
+            for (var i = 0; i < _floorList.length; ++i) {
+
+                if (_floorList[i].id === floorId) {
+
+                    return _floorList[i].floorIndex
+                }
+            }
+
+            return -1
+        }
+
+        function getFloorId(floorIndex) {
+
+            for (var i = 0; i < _floorList.length; ++i) {
+
+                if (_floorList[i].floorIndex === floorIndex) {
+
+                    return _floorList[i].id
+                }
+            }
+
+            return null
+        }
 
         /**
          * @param start 起点
@@ -19,9 +47,21 @@ define(function (require, exports, module) {
          * @param car 是否车行
          * @return PathResult
          */
-        this.router = function(start, end, car) {
+        this.routerPath = function(start, end, car) {
 
-            var result = _pathSearch.search(start.floorId, start.x, start.y, end.floorId, end.x, end.y, car, null)
+            var _sIndex = getFloorIndex(start.floorId)
+
+            var _eIndex = getFloorIndex(end.floorId)
+
+            var result = _pathSearch.search(_sIndex, start.x, start.y, _eIndex, end.x, end.y, car, null)
+
+            for (var i = 0; i < result.paths.length; ++i) {
+
+                for (var j = 0; j < result.paths[i].position.length; ++j) {
+
+                    result.paths[i].position[j].floorId = getFloorId(result.paths[i].position[j].floorIndex)
+                }
+            }
 
             return result
         }
