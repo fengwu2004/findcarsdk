202be237719a6ca469f54238929372d2d77175e5
ky <fengwu2006@gmail.com>
2017-04-20 13:32:32 +0800

    test

--- modules/IDRMapMarker/IDRMapMarker.js
+++ modules/IDRMapMarker/IDRMapMarker.js
@@ -30,9 +30,9 @@ define(function (require, exports, module) {
 
             image.setAttribute('y', 20)
 
-            image.setAttribute('width', '150')
-
-            image.setAttribute('height', '46')
+            // image.setAttribute('width', '150')
+            //
+            // image.setAttribute('height', '46')
 
             image.href.baseVal = 'http://img.htmleaf.com/1506/logo.png'
             // image.setAttribute('xlink:href', 'http://img.htmleaf.com/1506/logo.png')
