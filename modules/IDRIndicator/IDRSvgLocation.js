a3b4919cbe65cdf945da7f842473d4ca99f8df16
ky <fengwu2006@gmail.com>
2017-04-27 17:34:27 +0800

    test

--- modules/IDRIndicator/IDRSvgLocation.js
+++ modules/IDRIndicator/IDRSvgLocation.js
@@ -111,9 +111,9 @@ define(function (require, exports, module) {
 
                 var mt = getTransArray(trans)
 
-                mt[4] = xOffsetX + mt[4].float
+                mt[4] = xOffsetX + parseFloat(mt[4])
 
-                mt[5] = xOffsetY + mt[5].float
+                mt[5] = xOffsetY + parseFloat(mt[5])
 
                 var trans = 'matrix(' + mt[0] + ',' + mt[1] + ',' + mt[2] + ',' + mt[3] + ',' + mt[4] + ',' + mt[5] + ')'
 
