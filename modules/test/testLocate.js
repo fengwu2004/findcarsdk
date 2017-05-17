47617c32122773fa83bfa3117b3bf52ea81a6bdc
晏立 <yanli@yellfun.com>
2017-05-17 10:34:36 +0800

    测试

--- modules/test/testLocate.js
+++ modules/test/testLocate.js
@@ -7,7 +7,7 @@ seajs.use(['http://wx.indoorun.com/indoorun/app/yanli/indoorun/test/idrLocateSer
 
         var locator = new idrLocateServer();
 
-        locator.start('14428254382730015', '14557583851000004', didLocateSuccess);
+        locator.start('14639999225470024', '14639999239840621', didLocateSuccess);
 
         function didLocateSuccess(str) {
 
--- modules/test/testMap.js
+++ modules/test/testMap.js
@@ -4,29 +4,27 @@
 
 seajs.use([
     'http://wx.indoorun.com/indoorun/app/yanli/indoorun/sdk/modules/idrMapView',
-    'http://wx.indoorun.com/indoorun/app/yanli/indoorun/sdk/modules/IDRMapMarker/IDRMapMarker'], function (idrMapView, idrMapMarker) {
+    'http://wx.indoorun.com/indoorun/app/yanli/indoorun/sdk/modules/IDRMapMarker/IDRMapMarker',
+    'http://wx.indoorun.com/indoorun/app/yanli/indoorun/sdk/modules/idrLocationServer'], function (idrMapView, idrMapMarker, idrLocationServer) {
 
     var regionId = '14639999225470024'
 
-    var idrCarMarker = idrMapMarker['IDRCarMarker']
+    var floorId = '14639999239840621'
 
     var map = new idrMapView()
-
-    map.addEventListener(map.eventTypes.onUnitClick, function(unit) {
-
-        var marker = new idrCarMarker(unit.getPos())
-
-        map.addMarker(marker)
-    })
-
-    map.addEventListener(map.eventTypes.onFloorChangeSuccess, function(data) {
-
-        console.log(data)
-    })
 
     map.addEventListener(map.eventTypes.onInitMapSuccess, function(regionEx) {
 
         map.changeFloor(regionEx.floorList[0].id)
+
+        var locator = new idrLocationServer();
+
+        locator.start(regionId, floorId, didLocateSuccess);
+
+        function didLocateSuccess(str) {
+
+            alert(str);
+        }
     })
 
     map.initMap('2b497ada3b2711e4b60500163e0e2e6b', 'main', regionId)
