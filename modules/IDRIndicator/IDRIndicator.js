d95761fcdc69d5b95ebd859e167ae139f34091bc
yanli@yellfun.com <Sh123456>
2017-06-02 10:47:27 +0800

    test

--- modules/IDRIndicator/IDRIndicator.js
+++ modules/IDRIndicator/IDRIndicator.js
@@ -167,14 +167,13 @@ define(function (require, exports, module) {
 
                 mt[5] = xOffsetY + parseFloat(mt[5])
 
-                if (isNaN(mt[4])) {
-
-                    console.log('当前位置：' + mt[4] + ', ' + mt[5])
-                }
-
                 var trans = 'matrix(' + mt[0] + ',' + mt[1] + ',' + mt[2] + ',' + mt[3] + ',' + mt[4] + ',' + mt[5] + ')'
 
                 rootDom.setAttribute('transform', trans)
+
+                lastPosition.x = mt[4]
+
+                lastPosition.y = mt[5]
 
                 ++count
 
