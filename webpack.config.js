e384ab0a4d66711e1167f9267c42836e314e248b
Unknown <fengwu2006@gmail.com>
2018-06-02 16:19:02 +0800

    save

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
 		rules: [
 			{
 				test: /\.js$/,
-				include: [resolve('../indoorunMap')],
+				include: [resolve('.'), ],
 				use:{
 					loader: 'babel-loader',
 					options: {
